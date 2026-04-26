b18b644fe040d8e140e79af6f0830bbe358090b1
Uppish4 <henrique.a.matunaga@gmail.com>
2026-04-26 20:40:17 -0300

    Initial Ball Prototype

---
+++ ball.java
@@ -0,0 +1,91 @@
+public class ball {
+    static final double bounce_factor = 0.7;
+    static final double min_X_speed = 1.0;
+    static final double min_Y_speed = 5.0;
+    static final double friction = 0.99;
+
+    private int x, y, diameter;
+
+    double x_vel, y_vel;
+
+    boolean is_touching_ground = false;
+
+    public ball(int x, int y, int diameter) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.x_vel = 0;
+        this.y_vel = 0;
+    }
+
+    public void update() {
+        x += x_vel;
+        y += y_vel;
+
+        if (y >= game.y_boundary - diameter) {
+            is_touching_ground = true;
+        }
+        else {
+            is_touching_ground = false;
+        }
+
+        if(!is_touching_ground){
+            y_vel += game.gravity;
+        }
+        else{
+            x_vel *= friction;
+            // We only apply friction when the ball is grounded
+            // Whenever the ball bounces, it stays grounded for a frame, reducing speed a bit
+            // Applying friction all the time makes the ball slow down too much
+        }
+
+        if(Math.abs(x_vel) < min_X_speed){
+            x_vel = 0;
+        }
+    }
+
+
+    public void bounceX(){
+        if(x <= 0){
+            x = 0;
+        }
+        if(x >= game.x_boundary - diameter){
+            x = game.x_boundary - diameter;
+        }
+
+        x_vel = -x_vel * bounce_factor; // Apply damping
+    }
+
+
+    public void bounceY(){
+        if(y <= 0){
+            y = 0;
+        }
+        if(y >= game.y_boundary - diameter){
+            y = game.y_boundary - diameter;
+        }
+        if(Math.abs(y_vel) < min_Y_speed){
+            y_vel = 0;
+        }
+
+        y_vel = -y_vel * bounce_factor; // Apply damping
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    public double getXVel() {
+        return x_vel;
+    }
+    public double getYVel() {
+        return y_vel;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+}
---
+++ game.java
@@ -0,0 +1,112 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class game extends JPanel implements MouseListener{
+
+    static final double gravity = 0.5;
+
+    static final int x_boundary = 800;
+    static final int y_boundary = 600;
+
+    ball ball = new ball(400, 50, 20);
+
+    int x_input, y_input;
+
+    public game(){
+        addMouseListener(this);
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(800, 600));
+
+        setFocusable(true);
+    }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ball Game");
+
+        game game = new game();
+        frame.add(game);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+        frame.setVisible(true);
+
+        while(true) {
+            game.update();
+        }
+    }
+
+    public void update() {
+        try {
+            Thread.sleep(16); // ~60 FPS
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(ball.getY() >= y_boundary || ball.getY() <= 0) {
+            ball.bounceY();
+        }
+        if(ball.getX() <= 0 || ball.getX() >= x_boundary - ball.getDiameter()) {
+            ball.bounceX();
+        }
+
+        // use mouse input to change ball trajectory
+        if(x_input != 0 || y_input != 0) {
+            applyMouseInput();
+        }
+        
+        ball.update();
+
+        // clears the console
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Ball position: (" + ball.getX() + ", " + ball.getY() + ")");
+        System.out.println("Ball velocity: (" + ball.getXVel() + ", " + ball.getYVel() + ")");
+
+        repaint();
+    }
+
+
+    public void applyMouseInput() {
+        int x_diff = x_input - (ball.getX());
+        int y_diff = y_input - (ball.getY());
+
+        ball.x_vel += x_diff * 0.1;
+        ball.y_vel += y_diff * 0.1;
+
+        x_input = 0;
+        y_input = 0;
+    }
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Desenha o ball branco
+        g.setColor(Color.WHITE);
+        g.fillOval(ball.getX(), ball.getY(), ball.getDiameter(), ball.getDiameter());
+    }
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        x_input = e.getX();
+        y_input = e.getY();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+}
