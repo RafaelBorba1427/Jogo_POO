7bec965d70b317b2917f33cf9a6e9fd4cde1d63e
Henrique <henrique.matunaga@unesp.br>
2026-06-08 18:51:30 -0300

    Added basic logic for point system

---
+++ pointSystem.java
@@ -0,0 +1,24 @@
+// Just the logic, made to be used inside the ball class or the main game loop.
+
+public class pointSystem {
+    private int points;
+
+    public pointSystem() {
+        this.points = 0;
+    }
+
+
+    // Placeholder values, ideally add point values to the objects themselves
+    public void addPoints(coisa obj) {
+        if (obj instanceof buff) {
+            points += 10;
+        }
+        else {
+            points += 1;
+        }
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
