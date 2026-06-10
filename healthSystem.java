c29cab0db5020873e9f1a26d8b5f2e44cce0e9e7
Henrique <henrique.matunaga@unesp.br>
2026-06-10 00:35:42 -0300

    Added heart sprite

--- healthSystem.java
+++ healthSystem.java
@@ -1,12 +1,25 @@
-// Dunno how you expected this to be implemented, made it as a subclass to be added either to ball or to the main game loop
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.math.*;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.Vector;
 
 
-public class healthSystem {
+public class healthSystem{
     private int max_hp;
     private int current_hp;
 
     // A bit redundant, but might be useful
     private boolean is_dead;
+
 
 
     public healthSystem(int max_hp) {
@@ -46,4 +59,30 @@ public class healthSystem {
     public boolean isDead() {
         return is_dead;
     }
+
+
+    // For testing purposes
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Health System Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        healthSystem health = new healthSystem(5);
+    }
+}
+
+class heart{
+    private BufferedImage sprite;
+
+    public heart() {
+        this.sprite = sprite;
+
+        try {
+            sprite = ImageIO.read(new File("spritesheet/heart.png"));
+            System.out.println("heart loaded: " + sprite); // ✅ null or not?
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("heart FAILED to load"); // ❌ path wrong?
+        }
+    }
+
 }
