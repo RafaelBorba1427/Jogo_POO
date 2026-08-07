f9cf746711a86cca092112372ab71351c4e3a2d6
Henrique <henrique.matunaga@unesp.br>
2026-08-07 14:27:43 -0300

    Converted music to wav
    I gave up

---
+++ musicMaster.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+import java.io.File;
+
+public class musicMaster {
+    public static void main(String[] args) {
+        System.out.println("Starting musicMaster...");
+        // Create a new instance of the musicMaster class
+    }
+}
+
